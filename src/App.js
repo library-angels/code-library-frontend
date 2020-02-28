37acd9616702282df8dac96ba5e3095c5f4d347a
Denis Ismail <denis.ismail@code.berlin>
2020-02-28 14:03:28 +0100

    Add Router to App.js and implement Navigation component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
 import React from 'react';
-import Users from './components/Users';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+import Navigation from './components/Navigation';
+import Dashboard from './components/Dashboard';
+import Account from './components/Account';
+import Category from './components/Category';
+import Logout from './components/Logout';
 
 function App() {
     return (
-        <div className="App">
-            <Users />
+        <div className="app">
+            <Router>
+                <Navigation />
+                <Switch>
+                    <Route exact path="/">
+                        <Dashboard />
+                    </Route>
+                    <Route exact path="/account">
+                        <Account />
+                    </Route>
+                    <Route path="/category/:department">
+                        <Category />
+                    </Route>
+                    <Route path="/logout">
+                        <Logout />
+                    </Route>
+                </Switch>
+            </Router>
         </div>
     );
 }
