980b9aa2b767f44007c5161b0dd467454df3a414
Denis Ismail <denis.ismail@code.berlin>
2020-03-05 18:45:31 +0100

    Implement Dashboard Carousel for Main Book Categories

--- src/components/Dashboard/index.js
+++ src/components/Dashboard/index.js
@@ -1,11 +1,30 @@
 import React from 'react';
 import Search from '../Search';
+import BookCarousel from '../BookCarousel';
+
+import library from '../../library.json';
 
 function Dashboard() {
+    const categories = library.reduce((acc, book) => {
+        const { designation } = book;
+
+        if (acc[designation] === undefined) {
+            acc[designation] = [];
+        }
+        acc[designation].push(book);
+
+        return acc;
+    }, {});
+
     return (
         <div>
             <Search />
-            Dashboard Component
+            {Object.keys(categories).map(category => (
+                <BookCarousel
+                    category={category}
+                    books={categories[category].slice(0, 10)}
+                />
+            ))}
         </div>
     );
 }
