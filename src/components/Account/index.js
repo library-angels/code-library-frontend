da8028b50636b4bf08e5246ebe9b85b9ac445e50
“wail <wailabuahmad@gmail.com>
2020-03-16 20:11:18 +0100

    created Account Component

--- src/components/Account/index.js
+++ src/components/Account/index.js
@@ -1,7 +1,20 @@
 import React from 'react';
+import ProfilePic from '../ProfilePic';
+
+import './account.css';
+import BorrowingList from '../BorrowingList';
+import WaitingList from '../WaitingList';
+import HistoryList from '../HistoryList';
 
 function Account() {
-    return <div>My account</div>;
+    return (
+        <div className="profilepic">
+            <ProfilePic />
+            <BorrowingList title="Borrowing" />
+            <WaitingList title="Waiting List" />
+            <HistoryList title="History" />
+        </div>
+    );
 }
 
 export default Account;
