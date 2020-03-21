26eeab40e90c3432f9eb31ecd747cf54140e4ecc
wailaa <wail.abuahmad@code.berlin>
2020-03-21 15:17:54 +0100

    button move with expanded window and adjusted box shadow values

--- src/components/Carousel/index.js
+++ src/components/Carousel/index.js
@@ -3,44 +3,48 @@ import { Box, Flex, Image, Collapse, Button } from '@chakra-ui/core';
 import data from '../../LibTestApi/LibTestApiJson.json';
 
 function Carousel(props) {
-    const NewData = data.slice(0, 4);
-
+    const NewData = data.slice(0, 5);
     const URL = 'https://library.code.berlin/';
     const [show, setShow] = React.useState(false);
     const handleToggle = () => setShow(!show);
 
     return (
-        <div >
-            <Box fontWeight={'bold'} m={[2]} >{ props.title}</Box>
-            <Collapse id="wail" startingHeight={"200px"}  endingHeight ={400} isOpen={show}>
-                <Flex  maxW={'980px'}  align="center" justify="center" m={[2]}>
-                    {NewData.map(book => {
+        <>
+            <Box fontWeight='bold' m={4} >{ props.title}</Box>
+            <Collapse  startingHeight={152}   isOpen={show} m={2} >
+                <Flex  maxW='760' m={2}   flexWrap="wrap" >
+                    {NewData.map((book) => {
                         return (
-                            <Flex key={book.id} m={[2]} width={'25%'}  >
-                                <Flex  width={'100%'}  >
-                                    <Image 
-                                    maxW={ '100%'} 
-                                    maxH={'140px'}
+                            <Flex  key={book.id} m={2} width='20%'  pb={[6,2]}>
+                                <Image 
+                                    
+                                    maxW='100%'
+                                    maxH='120px'
                                     src={URL + book.cover}
-                                    boxShadow="   0 2.8px 2.2px rgba(0, 0, 0, 0.034),
+                                    boxShadow="     0 2.8px 2.2px rgba(0, 0, 0, 0.034),
                                                     0 6.7px 5.3px rgba(0, 0, 0, 0.048),
-                                                    0 12.5px 10px rgba(0, 0, 0, 0.06),
-                                                    0 22.3px 17.9px rgba(0, 0, 0, 0.072),
-                                                    0 41.8px 33.4px rgba(0, 0, 0, 0.086),
-                                                    0 100px 80px rgba(0, 0, 0, 0.12)" 
-                                    />
-                                </Flex>
+                                                    0 12.5px 10px rgba(0, 0, 0, 0.06)                                                
+                                                   " 
+                                />
                             </Flex>
-                        );
-                    })}
-                </Flex>
-                <Flex flexDirection='row-reverse'>
-                <Button  size="sm" variant="solid" onClick={handleToggle} >
-                    Show {show ? "Less" : "More"}
-                </Button>
+                        )
+                    })} 
                 </Flex>
             </Collapse>
-        </div>
+            {NewData.length<5 ? (
+                null
+            ) : (
+                <Flex flexDirection='row-reverse' ml={2}>
+                <Flex pl={["15px","19px","0px"]} width={'32%'}>
+                    <Button  size="xs"  maxW='100px' variant="solid" onClick={handleToggle} >
+                        Show {show ? "Less" : "More"}
+                    </Button>
+                </Flex>
+            </Flex>   
+            )}
+
+           
+        </>
     );
 }
 
