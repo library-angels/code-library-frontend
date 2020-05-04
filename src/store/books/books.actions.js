32fd55819a3cf3c2935ae2ad3abca438bf62cdad
denis <denis.ismail@code.berlin>
2020-05-04 16:53:40 +0200

    Change books action creators

--- src/store/books/books.actions.js
+++ src/store/books/books.actions.js
@@ -1,40 +1,57 @@
-const REQUEST_DASHBOARD_IDS = 'BOOKS_REQUEST_DASHBOARD_IDS';
-const RECEIVE_DASHBOARD_IDS = 'BOOKS_RECEIVE_DASHBOARD_IDS';
-const REQUEST_ALL = 'BOOKS_REQUEST_ALL';
-const RECEIVE_ALL = 'BOOKS_RECEIVE_ALL';
+const REQUEST = 'BOOKS_REQUEST';
+const RECEIVE = 'BOOKS_RECEIVE';
+const REQUEST_DESIGNATIONS = 'BOOKS_REQUEST_DESIGNATIONS';
+const RECEIVE_DESIGNATIONS = 'BOOKS_RECEIVE_DESIGNATIONS';
+const REQUEST_DESIGNATION_GROUPS = 'BOOKS_REQUEST_DESIGNATION_GROUPS';
+const RECEIVE_DESIGNATION_GROUPS = 'BOOKS_RECEIVE_DESIGNATION_GROUPS';
 
-const requestDashboardIDs = () => ({
-    type: REQUEST_DASHBOARD_IDS,
+const request = () => ({
+    type: REQUEST,
 });
 
-const receiveDashboardIDs = dashboardIDs => ({
-    type: RECEIVE_DASHBOARD_IDS,
-    payload: {
-        dashboardIDs,
-    },
-});
-
-const requestAll = () => ({
-    type: REQUEST_ALL,
-});
-
-const receiveAll = books => ({
-    type: RECEIVE_ALL,
+const receive = books => ({
+    type: RECEIVE,
     payload: {
         books,
     },
 });
 
+const requestDesignations = () => ({
+    type: REQUEST_DESIGNATIONS,
+});
+
+const receiveDesignations = designations => ({
+    type: RECEIVE_DESIGNATIONS,
+    payload: {
+        designations,
+    },
+});
+
+const requestDesignationGroups = () => ({
+    type: REQUEST_DESIGNATION_GROUPS,
+});
+
+const receiveDesignationGroups = groups => ({
+    type: RECEIVE_DESIGNATION_GROUPS,
+    payload: {
+        groups,
+    },
+});
+
 export const BOOKS_ACTIONS = {
-    REQUEST_ALL,
-    RECEIVE_ALL,
-    REQUEST_DASHBOARD_IDS,
-    RECEIVE_DASHBOARD_IDS,
+    REQUEST,
+    RECEIVE,
+    REQUEST_DESIGNATIONS,
+    RECEIVE_DESIGNATIONS,
+    REQUEST_DESIGNATION_GROUPS,
+    RECEIVE_DESIGNATION_GROUPS,
 };
 
 export const BOOKS_ACTION_CREATORS = {
-    requestDashboardIDs,
-    receiveDashboardIDs,
-    requestAll,
-    receiveAll,
+    request,
+    receive,
+    requestDesignations,
+    receiveDesignations,
+    requestDesignationGroups,
+    receiveDesignationGroups,
 };
