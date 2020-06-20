fc76ca012cb6be3e35be0f02b56674e444c0ae89
wailaa <wail.abuahmad@code.berlin>
2020-06-20 18:04:36 +0200

    refactored rootSaga

--- src/store/saga.js
+++ src/store/saga.js
@@ -1,4 +1,4 @@
-import { fork } from 'redux-saga/effects';
+import { all, fork } from 'redux-saga/effects';
 
 import { watcher } from './books/books.sagas';
 import { gaipSaga } from './GAPI/GAPI.sagas';
@@ -6,9 +6,11 @@ import { loginSaga } from './login/login.sagas';
 import { logoutSaga } from './logout/logout.sagas';
 
 const rootSaga = function* root() {
-    yield fork(watcher);
-    yield fork(gaipSaga);
-    yield fork(loginSaga);
-    yield fork(logoutSaga);
+    yield all([
+        fork(watcher),
+        fork(gaipSaga),
+        fork(loginSaga),
+        fork(logoutSaga),
+    ]);
 };
 export default rootSaga;
